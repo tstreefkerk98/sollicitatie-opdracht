788ab8b9d73972bc54b68a427afe22f860a5100a
Thomas <thomas.streefkerk@code-orange.nl>
2020-12-23 10:49:10 +0100

    Adds assignment.ts which holds the Assignment interface. Adds mock-assignments.ts, which holds the assignment objects. Creates assignments component and assignment service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentsComponent
   ],
   imports: [
     BrowserModule
